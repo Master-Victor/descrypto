4f02a57e5f25348ba2ae4fb7bf31eb0bd79459ae
victorvillafane <victor.villafane@davinci.edu.ar>
2024-10-29 14:37:49 -0300

    Pais - CRUD - Se agregaron validaciones en los endpoints

--- src/main/java/rest/decrypto/api_rest_decrypto/controllers/PaisController.java
+++ src/main/java/rest/decrypto/api_rest_decrypto/controllers/PaisController.java
@@ -1,6 +1,7 @@
 package rest.decrypto.api_rest_decrypto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rest.decrypto.api_rest_decrypto.models.Pais;
@@ -21,26 +22,47 @@ public class PaisController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Pais> getPaisById(@PathVariable Long id) {
-        Pais pais = paisService.getPaisById(id);
-        return ResponseEntity.ok(pais);
+    public ResponseEntity<?> getPaisById(@PathVariable Long id) {
+        try {
+            Pais pais = paisService.getPaisById(id);
+            return ResponseEntity.ok(pais);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al obtener el pais: " + e.getMessage());
+        }
+
     }
 
     @PostMapping
-    public ResponseEntity<Pais> createPais(@RequestBody Pais pais) {
-        Pais newPais = paisService.createPais(pais);
-        return ResponseEntity.ok(newPais);
+    public ResponseEntity<?> createPais(@RequestBody Pais pais) {
+        try {
+            Pais newPais = paisService.createPais(pais);
+            return ResponseEntity.ok(newPais);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al crear el pais: " + e.getMessage());
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Pais> updatePais(@PathVariable Long id, @RequestBody Pais pais) {
-        Pais updatedPais = paisService.updatePais(id, pais);
-        return ResponseEntity.ok(updatedPais);
+    public ResponseEntity<?> updatePais(@PathVariable Long id, @RequestBody Pais pais) {
+        try {
+            Pais updatedPais = paisService.updatePais(id, pais);
+            return ResponseEntity.ok(updatedPais);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al actualizar el pais: " + e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePais(@PathVariable Long id) {
-        paisService.deletePais(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<?> deletePais(@PathVariable Long id) {
+        try {
+            paisService.deletePais(id);
+            return ResponseEntity.noContent().build();
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al eliminar el pais: " + e.getMessage());
+        }
     }
 }
