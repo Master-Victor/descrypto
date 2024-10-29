26381f9770008a2fb4a75cc709bdf5e5c922ea79
victorvillafane <victor.villafane@davinci.edu.ar>
2024-10-29 14:52:44 -0300

    Mercado - CRUD - Se agregaron validaciones en los endpoints

--- src/main/java/rest/decrypto/api_rest_decrypto/controllers/MercadoController.java
+++ src/main/java/rest/decrypto/api_rest_decrypto/controllers/MercadoController.java
@@ -1,6 +1,7 @@
 package rest.decrypto.api_rest_decrypto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rest.decrypto.api_rest_decrypto.models.Mercado;
@@ -21,26 +22,46 @@ public class MercadoController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Mercado> getMercadoById(@PathVariable Long id) {
-        Mercado mercado = mercadoService.getMercadoById(id);
-        return ResponseEntity.ok(mercado);
+    public ResponseEntity<?> getMercadoById(@PathVariable Long id) {
+        try {
+            Mercado mercado = mercadoService.getMercadoById(id);
+            return ResponseEntity.ok(mercado);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al obtener el mercado: " + e.getMessage());
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Mercado> createMercado(@RequestBody Mercado mercado) {
-        Mercado newMercado = mercadoService.createMercado(mercado);
-        return ResponseEntity.ok(newMercado);
+    public ResponseEntity<?> createMercado(@RequestBody Mercado mercado) {
+        try {
+            Mercado newMercado = mercadoService.createMercado(mercado);
+            return ResponseEntity.ok(newMercado);
+        }catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al crear el mercado: " + e.getMessage());
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Mercado> updateMercado(@PathVariable Long id, @RequestBody Mercado mercado) {
-        Mercado updatedMercado = mercadoService.updateMercado(id, mercado);
-        return ResponseEntity.ok(updatedMercado);
+    public ResponseEntity<?> updateMercado(@PathVariable Long id, @RequestBody Mercado mercado) {
+        try {
+            Mercado updatedMercado = mercadoService.updateMercado(id, mercado);
+            return ResponseEntity.ok(updatedMercado);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al actualizar el mercado: " + e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteMercado(@PathVariable Long id) {
-        mercadoService.deleteMercado(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<?> deleteMercado(@PathVariable Long id) {
+        try {
+            mercadoService.deleteMercado(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al eliminar el mercado: " + e.getMessage());
+        }
     }
 }
