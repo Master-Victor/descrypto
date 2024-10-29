e979af320d0d8af71f28493c592ec204aa065f23
victorvillafane <victor.villafane@davinci.edu.ar>
2024-10-29 12:35:22 -0300

    Init

---
+++ src/main/java/rest/decrypto/api_rest_decrypto/ApiRestDecryptoApplication.java
@@ -0,0 +1,13 @@
+package rest.decrypto.api_rest_decrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDecryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDecryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rest/decrypto/api_rest_decrypto/ApiRestDecryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.decrypto.api_rest_decrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDecryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
